fcdd786c2ae2645dae3ffcfc20f189b5e89322ac
guanzhb <1127254428@qq.com>
2022-09-02 23:17:48 +0800

    [Optimize] return as soon as possible if false
    Signed-off-by: guanzhb <1127254428@qq.com>

--- cpfw/base/src/Utils.cpp
+++ cpfw/base/src/Utils.cpp
@@ -27,27 +27,29 @@ std::vector<int32_t> parseProfile(
         const Profile &profile, uint32_t type,
         std::string context, std::shared_ptr<DataStore> store) {
     std::vector<int32_t> ret;
+    if (!store) {
+        return ret;
+    }
     auto &elements = profile.elements;
     std::for_each(elements.begin(), elements.end(),
         [&](auto &data) -> void {
-            int32_t current = 0;
-            if (0 != (type & static_cast<uint32_t>(ElementType::PUBLIC))
-                    && (0 != (data.second.type &
+            if (0 == (type & static_cast<uint32_t>(ElementType::PUBLIC))
+                    || (0 == (data.second.type &
                               static_cast<uint32_t>(ElementType::PUBLIC)))) {
-                current = data.second.current;
-                if (store
-                        && 0 != (data.second.type
-                                 & static_cast<uint32_t>(ElementType::NEED_CONVERT))) {
-                    current = store->getConvertedData(context, current);
-                    auto& converts = store->getConvertTable(context);
-                    for (auto &c : converts) {
-                        current = StrategyCalculatePool::getStrategy(
-                                        c.getExpression())->handle(
-                                            context, current, c, store);
-                    }
-                }
-                ret.push_back(current);
+                return;
             }
+            int32_t current = data.second.current;
+            if (0 != (data.second.type
+                    & static_cast<uint32_t>(ElementType::NEED_CONVERT))) {
+                current = store->getConvertedData(context, current);
+                auto& converts = store->getConvertTable(context);
+                for (auto &c : converts) {
+                    current = StrategyCalculatePool::getStrategy(
+                                    c.getExpression())->handle(
+                                        context, current, c, store);
+                }
+            }
+            ret.push_back(current);
     });
     return ret;
 }
